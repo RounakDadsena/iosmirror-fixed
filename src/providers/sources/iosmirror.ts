36c9a9e9fe64e76c2af3f7f28f34eb25669cb737
Rounakdadsena <87689078+RounakDadsena@users.noreply.github.com>
2025-03-05 22:54:09 +0530

    Update iosmirror.ts

--- src/providers/sources/iosmirror.ts
+++ src/providers/sources/iosmirror.ts
@@ -162,7 +162,7 @@ const universalScraper = async (ctx: ShowScrapeContext | MovieScrapeContext): Pr
   if (!autoFile) autoFile = playlistRes[0].sources[0]?.file;
   if (!autoFile) throw new Error('Failed to fetch playlist');
 
-  const playlist = `https://prox-beige.vercel.app/m3u8-proxy?url=${encodeURIComponent(`${baseUrl}${autoFile}`)}&headers=${encodeURIComponent(JSON.stringify({ referer: baseUrl, cookie: makeCookieHeader({ hd: 'on' }) }))}`;
+  const playlist = `https://m3u8-3.wafflehacker.io/proxy?url=${encodeURIComponent(`${baseUrl}${autoFile}`)}&headers=${encodeURIComponent(JSON.stringify({ referer: baseUrl, cookie: makeCookieHeader({ hd: 'on' }) }))}`;
   ctx.progress(90);
 
   return {
